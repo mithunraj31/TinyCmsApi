5f7249a189d3ac4840e51d14271b4624eac4bbcf
Pongpeera Kundilokborirak <pongpeera.kun@gmail.com>
2020-07-14 11:01:27 +0900

    implemented vehicle summary APIs. created view entity. created DTOs. added new controllers. added new service methods.

--- src/main/java/com/cms/controller/VehicleController.java
+++ src/main/java/com/cms/controller/VehicleController.java
@@ -1,4 +1,5 @@
 package com.cms.controller;
+
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,11 +13,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cms.config.TokenProvider;
+import com.cms.dto.DailyOnlineDto;
+import com.cms.dto.HourlyOnlineDto;
 import com.cms.dto.VehicleDto;
 import com.cms.serviceimpl.VehicleServiceImpl;
 
@@ -30,29 +34,92 @@ public class VehicleController {
 	private VehicleServiceImpl vehicleService;
 	@Autowired
 	private TokenProvider tokenProvider;
+
 	@RequestMapping("")
-	public ResponseEntity<?> getAllVehiclesByUser(@RequestParam(required = false) String customer,HttpServletRequest request, Authentication auth){
+	public ResponseEntity<?> getAllVehiclesByUser(@RequestParam(required = false) String customer,
+			HttpServletRequest request, Authentication auth) {
 		String email = auth.getName();
 		// Get Stk_User id from token
 		String token = request.getHeader(HEADER_STRING);
-		token = token.replace(TOKEN_PREFIX,"");
-		Claims user = tokenProvider.getAllClaimsFromToken(token);
-		String stk_user = user.get("stk_user", String.class);
-		
+		token = token.replace(TOKEN_PREFIX, "");
+		String stk_user = this.getStkUserFromToken(token);
+
 		// Handle query parameter customer
-		if(customer!=null && request.isUserInRole("ROLE_ADMIN")==false) {
+		if (customer != null && request.isUserInRole("ROLE_ADMIN") == false) {
 			return new ResponseEntity<Error>(HttpStatus.FORBIDDEN);
-		} else if (customer!=null && request.isUserInRole("ROLE_ADMIN")==true){
+		} else if (customer != null && request.isUserInRole("ROLE_ADMIN") == true) {
 			stk_user = customer;
-		}     
-		
+		}
+
 		List<VehicleDto> vehicles = this.vehicleService.getAllVehiclesByUser(stk_user);
-		Map <String, Object> response = new LinkedHashMap<>();
+		Map<String, Object> response = new LinkedHashMap<>();
 		response.put("email", email);
-		response.put("stk_user",stk_user);
+		response.put("stk_user", stk_user);
 		response.put("total", vehicles.size());
 		response.put("vehicles", vehicles);
-		return new ResponseEntity<Map<String,Object>>(response, HttpStatus.OK);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+
+	}
+
+	/**
+	 * request GET /api/vehicle/online
+	 */
+	@GetMapping("/online")
+	public ResponseEntity<?> getOnlineVehicle(HttpServletRequest request) {
+
+		// get stk_user from JWT in HTTP request headers
+		String token = request.getHeader(HEADER_STRING);
+		String stk_user = this.getStkUserFromToken(token);
+
+		// if user is admin, set empty value to the variable
+		// because will search from all users
+		stk_user = stk_user.toLowerCase().equals("admin") ? "" : stk_user;
+		long online = this.vehicleService.getOnlineVehicle(stk_user);
+		Map<String, Object> response = new LinkedHashMap<>();
+
+		response.put("online", online);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK); 
+	}
+
+	/**
+	 * request GET /api/vehicle/online?days={0 ~ N}
+	 */
+	@GetMapping("/online/summary")
+	public ResponseEntity<?> getOnlineSummary(@RequestParam(required = false) int days,
+		HttpServletRequest request) {
 		
+		// get stk_user from JWT in HTTP request headers
+		String token = request.getHeader(HEADER_STRING);
+		String stk_user = this.getStkUserFromToken(token);
+
+			// if user is admin, set empty value to the variable
+		// because will search from all users
+		stk_user = stk_user.toLowerCase().equals("admin") ? "" : stk_user;
+
+		Map<String, Object> response = new LinkedHashMap<>();
+		// if days is only one day will get hourly data
+		if (days == 1) {
+			List<HourlyOnlineDto> reports =  this.vehicleService.getHourlyOnlineDevice(stk_user);
+			response.put("reports", reports);
+		} else {
+			List<DailyOnlineDto> reports =  this.vehicleService.getDailyOnlineDevice(days, stk_user);
+			response.put("reports", reports);
+		}
+
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK); 
+	}
+
+	/**
+	 * get stk_user value from JWY payload
+	 * @param token JWT in HTTP request headers
+	 * @return String value of stk_user.
+	 */
+	private String getStkUserFromToken(String token) {
+		// remove text "Bearer " from header value
+		token = token.replace(TOKEN_PREFIX, "");
+		// get payload as Claims object
+		Claims user = tokenProvider.getAllClaimsFromToken(token);
+		// get value by object key
+		return user.get("stk_user", String.class);
 	}
 }
--- src/main/java/com/cms/serviceimpl/VehicleServiceImpl.java
+++ src/main/java/com/cms/serviceimpl/VehicleServiceImpl.java
@@ -1,28 +1,141 @@
 package com.cms.serviceimpl;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.StoredProcedureQuery;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cms.dto.DailyOnlineDto;
+import com.cms.dto.HourlyOnlineDto;
 import com.cms.dto.VehicleDto;
 import com.cms.http.VehicleHttp;
+import com.cms.model.view.OnlineDevice;
 
 @Service("VehicleServiceImpl")
 public class VehicleServiceImpl {
-@Autowired
-private VehicleHttp vehicleHttp;
+	@Autowired
+	private VehicleHttp vehicleHttp;
 
-public List<VehicleDto> getAllVehiclesByUser(String user) {
-	List<VehicleDto> vehicleList = new ArrayList<>();
-	try {
-		vehicleList = vehicleHttp.getAllVehicles(user);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	@PersistenceContext
+    private EntityManager entityManager;
+
+	public List<VehicleDto> getAllVehiclesByUser(String user) {
+		List<VehicleDto> vehicleList = new ArrayList<>();
+		try {
+			vehicleList = vehicleHttp.getAllVehicles(user);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return vehicleList;
 	}
-	return vehicleList;
-}
+
+	/**
+	 * get number of online vehicle from event database table.
+	 * @param user stk_user from kitting server 
+	 * @return long number of online vehicle
+	 */
+	public long getOnlineVehicle(String user) {
+		Query query = null;
+		// user has not value will calculate all vehicle
+		if (user != null && !user.isEmpty()){
+			query = this.entityManager.createQuery("SELECT v FROM OnlineDevice v WHERE v.userName = :username", OnlineDevice.class);
+			query.setParameter("username", user);
+		} else {
+			query = this.entityManager.createQuery("SELECT v FROM OnlineDevice v", OnlineDevice.class);
+		}
+		
+		List<OnlineDevice> results = query.getResultList();
+
+		return results.size();
+	}
+
+	/**
+	 * get online vehicle by day range.
+	 * @param days day's search range  
+	 * @param user stk_user from kitting server 
+	 * @return List<DailyOnlineDto> result object contians day and number of online vehicle each object
+	 */
+	public List<DailyOnlineDto> getDailyOnlineDevice(int days, String user) {
+		StoredProcedureQuery query = 
+			this.entityManager
+			.createStoredProcedureQuery("get_daily_online");
+
+		// register stored procedure parameter.
+		query.registerStoredProcedureParameter(1, Integer.class, ParameterMode.IN);
+		query.registerStoredProcedureParameter(2, String.class, ParameterMode.IN);
+
+		// set parameter value
+		query.setParameter(1, days);
+		query.setParameter(2, user);
+		List<Object[]> results = query.getResultList();
+		List<DailyOnlineDto> dailyOnlineDtoList = new ArrayList<DailyOnlineDto>();
+
+		// map result objects to class model
+		results.stream().forEach((record) -> {
+			Object day = record[0];
+			Object online = record[1];
+			dailyOnlineDtoList.add(
+				new DailyOnlineDto(this.formatStringDateFormObject(day), 
+				((BigInteger)online).longValue()));
+		});
+		
+		return dailyOnlineDtoList;
+	}
+
+	/**
+	 * get number of online vehicle by hour range in 24 hour
+	 * @param user stk_user from kitting server 
+	 * @return List<HourlyOnlineDto> result object contians day, hour, and number of online vehicle each object
+	 */
+	public List<HourlyOnlineDto> getHourlyOnlineDevice(String user) {
+		StoredProcedureQuery query = 
+			this.entityManager
+			.createStoredProcedureQuery("get_one_day_hourly_online");
+
+		query.registerStoredProcedureParameter(1, String.class, ParameterMode.IN);
+		query.setParameter(1, user);
+
+		List<Object[]> results = query.getResultList();
+		List<HourlyOnlineDto> hourlyOnlineDtoList = new ArrayList<HourlyOnlineDto>();
+		results.stream().forEach((record) -> {
+			Object day = record[0];
+			Object hour = record[1];
+			Object online = record[2];
+			hourlyOnlineDtoList.add(new HourlyOnlineDto(
+				this.formatStringDateFormObject(day), 
+				(int) hour,
+				((BigInteger)online).longValue()));
+		});
+		
+		return hourlyOnlineDtoList;
+	}
+
+	/**
+	 * cast date object to string in format yyyy-MM-dd
+	 * @param dateObj Object class contain date value
+	 * @return String if the object is string  will return string date 
+	 * if not string will return empty string
+	 */
+	private String formatStringDateFormObject(Object dateObj) {
+		
+		if (dateObj instanceof java.util.Date) {
+			String pattern = "yyyy-MM-dd";
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+			java.util.Date date = (java.util.Date) dateObj;
+
+			return simpleDateFormat.format(date);
+		}
+		return "";
+	}
 }
