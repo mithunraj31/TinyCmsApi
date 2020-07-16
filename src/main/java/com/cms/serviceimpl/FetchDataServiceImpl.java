23343fa9282f491f5412804379aae5d744afd48b
Pongpeera Kundilokborirak <pongpeera.kun@gmail.com>
2020-07-16 14:51:43 +0900

    improved scheduler data migration. Seperated CronJobConfig from WebSecurityConfig.

--- src/main/java/com/cms/serviceimpl/FetchDataServiceImpl.java
+++ src/main/java/com/cms/serviceimpl/FetchDataServiceImpl.java
@@ -3,6 +3,7 @@ package com.cms.serviceimpl;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.cms.dao.CustomerDao;
@@ -15,7 +16,6 @@ import com.cms.model.CustomerModel;
 import com.cms.model.DeviceModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service("FetchDataServiceImpl")
@@ -32,16 +32,6 @@ public class FetchDataServiceImpl {
     @Autowired
     private VehicleHttp vehicleService;
 
-    @Scheduled(fixedDelay = 1000 * 60 * 10, initialDelay = 1000)
-    private void fetchCustomerData() {
-        this.syncCustomerData();
-    }
-
-    @Scheduled(fixedDelay = 1000 * 60 * 10, initialDelay = 10000)
-    private void fetchDeviceData() {
-        this.syncDeviceData();
-    }
-
     public Boolean syncCustomerData() {
         List<CustomerDto> customerList;
         try {
@@ -49,62 +39,67 @@ public class FetchDataServiceImpl {
         } catch (IOException e1) {
             return false;
         }
-        for (int i = 0; customerList.size() > i; i++) {
+        final List<CustomerModel> customerModels = new ArrayList<>();
+        for (CustomerDto customerDto : customerList) {
             final CustomerModel customer = new CustomerModel();
-            customer.setId(customerList.get(i).getId());
-            customer.setSid(customerList.get(i).getSid());
-            customer.setStkUser(customerList.get(i).getStk_user());
-            customer.setDescription(customerList.get(i).getDescription());
-            try {
-                customerDao.save(customer);
+            customer.setId(customerDto.getId());
+            customer.setSid(customerDto.getSid());
+            customer.setStkUser(customerDto.getStk_user());
+            customer.setDescription(customerDto.getDescription());
+            customerModels.add(customer);
+        }
+        try {
+            customerDao.saveAll(customerModels);
 
-            } catch (final Exception e) {
-                return false;
-            }
+        } catch (final Exception e) {
+            return false;
         }
         return true;
     }
 
     public Boolean syncDeviceData() {
         final List<CustomerModel> customers = this.customerDao.findAllByOrderByStkUserAsc();
-        for (int i = 0; customers.size() > i; i++) {
+
+        List<DeviceModel> toSaveDevices = new ArrayList<>();
+        for (CustomerModel customer : customers) {
             List<VehicleDto> devices;
             try {
-                devices = this.vehicleService.getAllVehicles(customers.get(i).getStkUser());
+                devices = this.vehicleService.getAllVehicles(customer.getStkUser());
             } catch (IOException e1) {
                 return false;
             }
-            for (int j = 0; devices.size() > j; j++) {
+
+            for (VehicleDto device : devices) {
                 DeviceModel toSaveDevice = new DeviceModel();
-                toSaveDevice.setActive(devices.get(j).getDetail().isIsActive());
-                toSaveDevice.setChannelNumber(devices.get(j).getDetail().getChannelNumber());
+                toSaveDevice.setActive(device.getDetail().isIsActive());
+                toSaveDevice.setChannelNumber(device.getDetail().getChannelNumber());
 
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                LocalDateTime createTime = LocalDateTime.parse(devices.get(j).getDetail().getCreateTime(), formatter);
+                LocalDateTime createTime = LocalDateTime.parse(device.getDetail().getCreateTime(), formatter);
 
                 toSaveDevice.setCreateTime(createTime);
-                toSaveDevice.setDeviceId(devices.get(j).getId().toString());
-                toSaveDevice.setDeviceType(devices.get(j).getDetail().getDeviceType());
-                toSaveDevice.setGroupName(devices.get(j).getDetail().getGroupName());
-                toSaveDevice.setNetType(devices.get(j).getDetail().getNetType());
-                toSaveDevice.setPlateNumber(devices.get(j).getDetail().getPlateNumber());
-                toSaveDevice.setScanCode(devices.get(j).getDetail().getScanCode());
-                toSaveDevice.setStkUser(customers.get(i).getStkUser());
-                toSaveDevice.setTcpServerAddr(devices.get(j).getDetail().getTcpServerAddr());
-                toSaveDevice.setTcpStreamOutPort(devices.get(j).getDetail().getTcpStreamOutPort());
-                toSaveDevice.setUdpServerAddr(devices.get(j).getDetail().getUdpServerAddr());
-                toSaveDevice.setUdpStreamOutPort(devices.get(j).getDetail().getUdpStreamOutPort());
+                toSaveDevice.setDeviceId(device.getId().toString());
+                toSaveDevice.setDeviceType(device.getDetail().getDeviceType());
+                toSaveDevice.setGroupName(device.getDetail().getGroupName());
+                toSaveDevice.setNetType(device.getDetail().getNetType());
+                toSaveDevice.setPlateNumber(device.getDetail().getPlateNumber());
+                toSaveDevice.setScanCode(device.getDetail().getScanCode());
+                toSaveDevice.setStkUser(customer.getStkUser());
+                toSaveDevice.setTcpServerAddr(device.getDetail().getTcpServerAddr());
+                toSaveDevice.setTcpStreamOutPort(device.getDetail().getTcpStreamOutPort());
+                toSaveDevice.setUdpServerAddr(device.getDetail().getUdpServerAddr());
+                toSaveDevice.setUdpStreamOutPort(device.getDetail().getUdpStreamOutPort());
                 toSaveDevice.setUpdate_time(LocalDateTime.now());
-
-                try {
-
-                    this.deviceDao.save(toSaveDevice);
-                } catch (Exception e) {
-                    return false;
-                }
+                toSaveDevices.add(toSaveDevice);
+                
             }
         }
 
+        try {
+            this.deviceDao.saveAll(toSaveDevices);
+        } catch (Exception e) {
+            return false;
+        }
         return true;
     }
 }
